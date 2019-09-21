12149df11ab6ad6902acd4878ffeb21fa2188b70
laizhenxing <845766579@qq.com>
2019-09-21 15:39:06 +0800

    init gin-blog

---
+++ go.mod
@@ -0,0 +1,13 @@
+module gin-blog
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/go-ini/ini v1.47.0
+	github.com/jinzhu/gorm v1.9.10
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/unknwon/com v1.0.1
+	google.golang.org/appengine v1.6.3 // indirect
+	gopkg.in/ini.v1 v1.47.0 // indirect
+)
---
+++ models/models.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"fmt"
+	"gin-blog/pkg/setting"
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+var db *gorm.DB
+
+type Model struct {
+	ID         int `gorm:"primary_key" json:"id"`
+	CreatedOn  int `json:"created_on"`
+	ModifiedOn int `json:"modified_on"`
+}
+
+func init() {
+	var (
+		err                                               error
+		dbType, dbName, user, password, host, tablePrefix string
+	)
+
+	sec, err := setting.Cfg.GetSection("database")
+	if err != nil {
+		log.Fatalf(2, "Fail to get section 'database': %v", err)
+	}
+
+	dbType = sec.Key("Type").String()
+	dbName = sec.Key("NAME").String()
+	user = sec.Key("USER").String()
+	password = sec.Key("PASSWORD").String()
+	host = sec.Key("host").String()
+	tablePrefix = sec.Key("TABLE_PREFIX").String()
+
+	db, err = gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf7&parseTime=True&loc=Local",
+		user,
+		password,
+		host,
+		dbName))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return tablePrefix + defaultTableName
+	}
+
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SexMaxOpenConns(100)
+}
+
+func CloseDB() {
+	defer db.Close()
+}
