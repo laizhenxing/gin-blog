df2285748bf90362e787057a2e2f66fbf71d1ffe
laizhenxing <845766579@qq.com>
2019-10-30 20:05:30 +0800

    使用endless包做信号量控制

--- main.go
+++ main.go
@@ -2,24 +2,40 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"syscall"
 
 	"gin-blog/pkg/setting"
 	"gin-blog/routers"
+	"github.com/fvbock/endless"
 )
 
-func main()  {
+func main() {
 	fmt.Println("exec main func")
 
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	//router := routers.InitRouter()
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	// 启动服务
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
+	//s := &http.Server{
+	//	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+	//	Handler:        router,
+	//	ReadTimeout:    setting.ReadTimeout,
+	//	WriteTimeout:   setting.WriteTimeout,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+	//
+	//s.ListenAndServe()
 }
